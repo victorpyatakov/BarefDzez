0b6f7470298a417a493097dea56ced568ad98292
piatakov.v <piatakov.v@selectel.ru>
2022-12-17 16:02:47 +0400

    change output str

--- exchangers/__init__.py
+++ exchangers/__init__.py
@@ -7,7 +7,7 @@ def get_current_exhange():
     result = {
         'INEKO': Ineko().get_RUB_AMD(),
         'SAS': Sas().get_RUB_AMD(),
-        'TINKOFF_Cash': Tinkoff().get_cash_atm_RUB_AMD() ,
+        'TINKOFF_MIR': Tinkoff().get_cash_atm_RUB_AMD() ,
         'TINKOFF_APP_Exchange': Tinkoff().get_app_RUB_AMD(),
     }
     current_exhange = json.dumps(result, sort_keys=True, indent=4)
